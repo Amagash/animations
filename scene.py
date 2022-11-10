837e5916217d67e86e357597c7336d6fd57a05cd
Amagash <tiffanysouterre@gmail.com>
2022-11-10 09:54:30 -0300

    square to rec

--- scene.py
+++ scene.py
@@ -7,6 +7,17 @@ class CreateCircle(Scene):
         circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
         self.play(Create(circle))  # show the circle on screen
 
+class SquareToRectangle(Scene):
+    def construct(self):
+        rectangle = Rectangle()
+        square = Square()  # create a square
+        
+        square.rotate(PI / 4)  # rotate a certain amount
+
+        self.play(Create(square))  # animate the creation of the square
+        self.play(Transform(square, rectangle))  # interpolate the square into the circle
+        self.play(FadeOut(rectangle))  # fade out animation
+
 class SquareToCircle(Scene):
     def construct(self):
         circle = Circle()  # create a circle
