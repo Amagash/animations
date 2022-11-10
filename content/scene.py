3f99b63beb7fbf090c5db428f3330d2690361a55
Amagash <tiffanysouterre@gmail.com>
2022-11-10 12:18:39 -0300

    moved scene

--- content/scene.py
+++ content/scene.py
@@ -7,16 +7,19 @@ class CreateCircle(Scene):
         circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
         self.play(Create(circle))  # show the circle on screen
 
+
 class SquareToRectangle(Scene):
     def construct(self):
         rectangle = Rectangle()
         square = Square()  # create a square
-        
+
         square.rotate(PI / 4)  # rotate a certain amount
 
         self.play(Create(square))  # animate the creation of the square
-        self.play(Transform(square, rectangle))  # interpolate the square into the circle
+        # interpolate the square into the circle
+        self.play(Transform(square, rectangle))
         self.play(FadeOut(rectangle))  # fade out animation
+
 
 class SquareToCircle(Scene):
     def construct(self):
@@ -27,8 +30,10 @@ class SquareToCircle(Scene):
         square.rotate(PI / 4)  # rotate a certain amount
 
         self.play(Create(square))  # animate the creation of the square
-        self.play(Transform(square, circle))  # interpolate the square into the circle
+        # interpolate the square into the circle
+        self.play(Transform(square, circle))
         self.play(FadeOut(square))  # fade out animation
+
 
 class SquareAndCircle(Scene):
     def construct(self):
@@ -40,6 +45,7 @@ class SquareAndCircle(Scene):
 
         square.next_to(circle, RIGHT, buff=0.5)  # set the position
         self.play(Create(circle), Create(square))  # show the shapes on screen
+
 
 class AnimatedSquareToCircle(Scene):
     def construct(self):
@@ -55,3 +61,20 @@ class AnimatedSquareToCircle(Scene):
             circle.animate.set_fill(PINK, opacity=0.5)
         )  # color the circle on screen
 
+
+class RegularPolygonExample(Scene):
+    def construct(self):
+        poly_1 = RegularPolygon(n=6)
+        poly_2 = RegularPolygon(n=6, start_angle=30*DEGREES, color=GREEN)
+        poly_3 = RegularPolygon(n=10, color=RED)
+
+        # poly_group = Group(poly_1, poly_2, poly_3).scale(1.5).arrange(buff=1)
+        # self.add(poly_group)
+        # self.wait(1)
+        poly_1.move_to(LEFT*2)
+        # poly_2.move_to(UP*2)
+        poly_3.move_to(RIGHT*2)
+
+        self.play(Create(poly_1))
+        self.play(Create(poly_2))
+        self.play(Create(poly_3))
