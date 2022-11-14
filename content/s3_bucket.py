bf4b544a7d29cf182c31c50f2dd90687e289aa74
Amagash <tiffanysouterre@gmail.com>
2022-11-14 10:15:41 -0300

    update s3 bucket

--- content/s3_bucket.py
+++ content/s3_bucket.py
@@ -4,6 +4,9 @@ from assets.building_blocks import *
 
 class S3Bucket(Scene):
     def construct(self):
+        for x in range(-7, 8):
+            for y in range(-4, 5):
+                self.add(Dot(np.array([x, y, 0]), color=DARK_GREY))
         s3_bucket = create_service(service_name="S3", service_color=PURPLE)
         vpc = create_ensemble(ensemble_name="VPC", ensemble_color=BLUE)
         self.play(Create(s3_bucket))
